745795dc66a8d1c40ff0a98dd71a5035887ddea4
wendellopes <wendelopes@msn.com>
2023-01-28 13:42:55 -0300

    add styled project

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -15,9 +15,7 @@ SimpleForm.setup do |config|
     b.use :html5
     b.use :placeholder
     b.use :label, class: 'hidden'
-    b.use :input, class: 'form-control'
-    b.use :error, wrap_with: { tag: :span, class: 'error' }
-    b.use :hint,  wrap_with: { tag: :span, class: 'hint' }
+    b.use :input, class: 'w-full'
   end
 
   # Default configuration
